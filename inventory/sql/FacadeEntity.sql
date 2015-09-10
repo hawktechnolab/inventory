a9eddc761f6deeab474ccf8a06de0e8f5ddeb096
Nisheeth Shah <nishith.shah@outlook.com>
2015-09-11 01:02:53 +0530

    sql/FacadeEntity.sql for default entries

---
+++ inventory/sql/FacadeEntity.sql
@@ -0,0 +1,2 @@
+--This file contains the default values to be inserted into facadeentity table
+insert into facadeentity (id, ejb, entity, remoteinterface) values (1, 'BrandEntityFacade','com.hawk.inventory.entity.BrandEntity', 'com.hawk.inventory.ejb.remote.BrandEntityFacadeRemote');
